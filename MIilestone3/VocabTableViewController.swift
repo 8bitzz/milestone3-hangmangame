ffa07f1af8f7bc38e6fe015c560a4a557f00d41b
8bitzz <hanu.nguyenhang@gmail.com>
2019-04-09 17:00:12 +0700

    add new vocabulary

--- MIilestone3/VocabTableViewController.swift
+++ MIilestone3/VocabTableViewController.swift
@@ -13,16 +13,40 @@ class VocabTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "Vocabulary"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        guard let vocab = vocabList else { return 1 }
+        return vocab.vocabList.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "VocabCell", for: indexPath)
+        guard let vocab = vocabList else { return UITableViewCell() }
+        cell.textLabel?.text = vocab.vocabList[indexPath.row].word
+        cell.detailTextLabel?.text = vocab.vocabList[indexPath.row].definition
         return cell
+    }
+    
+    @objc func addButtonTapped() {
+        let ac = UIAlertController(title: "New vocabulary", message: nil, preferredStyle: .alert)
+        ac.addTextField { (wordName) in
+            wordName.placeholder = "Enter a new word here"
+        }
+        ac.addTextField { (wordDefinition) in
+            wordDefinition.placeholder = "Enter short meaning here"
+        }
+        let addAction = UIAlertAction(title: "OK", style: .default) { [weak self, weak ac] action in
+            guard let wordName = ac?.textFields?[0].text,
+            let wordDefinition = ac?.textFields?[1].text else { return }
+            self?.vocabList?.add(newVocab: Vocab(word: wordName, definition: wordDefinition))
+            self?.tableView.reloadData()
+        }
+        ac.addAction(addAction)
+        present(ac, animated: true)
     }
 
 }
